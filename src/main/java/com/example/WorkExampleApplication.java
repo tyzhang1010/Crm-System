7b2d43d70485ae1f8318619032fea4e70bdcbab3
Ty <yuzhang1010@gmail.com>
2023-10-26 09:33:43 +0800

    Initial commit

---
+++ src/main/java/com/example/WorkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.workexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workexample/WorkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
